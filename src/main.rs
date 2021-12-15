8446842f4564693b4ae544f8b6caea114a20bcc6
XaydBayeck <SidBayeck@outlook.com>
2021-12-15 17:54:48 +0800

    Mount static route and the interfaces of index page and fivicon

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,41 @@
-fn main() {
-    println!("Hello, world!");
+use rocket::{fs::FileServer, response::Redirect};
+
+#[macro_use] extern crate rocket;
+
+// static
+
+// Index
+#[get("/")]
+fn index() -> Redirect {
+    Redirect::to(uri!("/public/index.html"))
 }
+
+#[get("/favicon.ico")]
+fn favicon() -> Redirect {
+    Redirect::to(uri!("/public/favicon.ico"))
+}
+
+// 404
+
+// About:
+
+// Home:
+
+// Tags:
+
+// Blogs:
+
+// Login:
+
+// UpMK:
+
+// TODO
+
+
+#[launch]
+fn rocket() -> _ {
+    rocket::build().mount("/", routes![favicon, index])
+        .mount("/public", FileServer::from("static"))
+}
+
+
